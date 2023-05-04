e9b7b3dfd93fe58a8cc3835d67c155463592164e
OEncouracado <tryironcl4d@gmail.com>
2023-05-04 18:48:35 -0300

    tentando criar uma UI pra add itens ao carrossel

--- src/components/carrocel/Carrocel.jsx
+++ src/components/carrocel/Carrocel.jsx
@@ -1,47 +1,60 @@
-import React, { useEffect } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
 import frente3 from "../../images/frente3.JPG";
-import mulher from "../../images/mulher.jpg";
+import mulher from "../../images/mulher.jpg"
 import emprev from "../../images/emprevcarosel.png";
-import "./carousel.css";
+import "./carousel.css"
+import { useState, useEffect } from 'react';
 
-export default function CarouselManagement() {
-  useEffect(() => {
-    const anoFundacao = '1989'
-    var data = new Date();
-    var ano = data.getFullYear();
-    var result = (ano) - (anoFundacao);
+export default function CarouselFade() {
+    const [idade, setIdade] = useState(0);
 
-    document.getElementById("idade").innerHTML = (result)
-  });
+    const getAnosFundacao = () => {
+        const anoFundacao = 1989;
+        const data = new Date();
+        const ano = data.getFullYear();
+        return ano - anoFundacao;
+    }
 
-  return (
-    <Carousel fade>
-      <Carousel.Item>
-        <img
-          className="d-block w-100 carousel-img"
-          src={mulher}
-          alt="First slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100 carousel-img"
-          src={emprev}
-          alt="Second slide"
-        />
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100 carousel-img"
-          src={frente3}
-          alt="Third slide"
-        />
-        <Carousel.Caption className="caption">
-          <h3>Mais que um Hospital, um novo conceito.</h3>
-          <p>Há <span id='idade' /> anos cuidando do coração da baixada.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
-  );
+    useEffect(() => {
+        setIdade(getAnosFundacao());
+    }, []);
+
+    const items = [
+        {
+            img: mulher,
+            alt: "First slide"
+        },
+        {
+            img: emprev,
+            alt: "Second slide"
+        },
+        {
+            img: frente3,
+            alt: "Third slide",
+            caption: {
+                title: "Mais que um Hospital, um novo conceito.",
+                text: `Há ${idade} anos cuidando do coração da baixada.`
+            }
+        },
+    ];
+
+    return (
+        <Carousel fade>
+            {items.map((item, index) => (
+                <Carousel.Item key={index}>
+                    <img
+                        className="d-block w-100 carousel-img"
+                        src={item.img}
+                        alt={item.alt}
+                    />
+                    {item.caption && (
+                        <Carousel.Caption className="caption">
+                            <h3>{item.caption.title}</h3>
+                            <p>{item.caption.text}</p>
+                        </Carousel.Caption>
+                    )}
+                </Carousel.Item>
+            ))}
+        </Carousel>
+    );
 }
--- src/pages/admin/index.jsx
+++ src/pages/admin/index.jsx
@@ -1,20 +1,68 @@
-import Footer2 from "../../components/footer2/Footer2";
-import Topbar from "../../components/topbar/Topbar";
-import { Helmet } from 'react-helmet';
-import CarouselManagement from "../../components/Manager/CarouselManager";
-
+import React, { useState } from "react";
+import CarouselFade from "../../components/carrocel/Carrocel.jsx";
+import "./style.css";
 
 export default function Adm() {
-  return(
-    <>
-    <Helmet>
-        <title>ADM - Hospital EMCOR</title>
-        <meta name="description" content="ADM PAGE" />
-    </Helmet>
-    <Topbar />
-    <CarouselManagement/>
-    <Footer2 />
-    
-    </>
-  );
+    const [imgFile, setImgFile] = useState(null);
+    const [imgUrl, setImgUrl] = useState("");
+    const [imgAlt, setImgAlt] = useState("");
+    const [captionTitle, setCaptionTitle] = useState("");
+    const [captionText, setCaptionText] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+
+        const formData = new FormData();
+        formData.append("file", imgFile);
+
+        fetch("/api/upload", {
+            method: "POST",
+            body: formData,
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            const newItem = {
+                img: data.url, // url da imagem enviada para o servidor
+                alt: imgAlt,
+                caption: {
+                    title: captionTitle,
+                    text: captionText,
+                },
+            };
+            CarouselFade.addNewItem(newItem);
+        });
+    };
+
+    const handleFileChange = (event) => {
+        setImgFile(event.target.files[0]);
+        setImgUrl(URL.createObjectURL(event.target.files[0]));
+    };
+
+    return (
+        <>
+            <h1>Adicionar novo slide</h1>
+            <form className="form-container" onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label htmlFor="imgFile">Enviar imagem:</label>
+                    <input type="file" id="imgFile" accept="image/*" onChange={handleFileChange} />
+                </div>
+                <div>
+                    <img src={imgUrl} alt="Preview" style={{ maxWidth: "100%", maxHeight: "300px" }} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="imgAlt">Texto alternativo da imagem:</label>
+                    <input type="text" id="imgAlt" value={imgAlt} onChange={(e) => setImgAlt(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="captionTitle">Título da descrição:</label>
+                    <input type="text" id="captionTitle" value={captionTitle} onChange={(e) => setCaptionTitle(e.target.value)} />
+                </div>
+                <div className="form-group">
+                    <label htmlFor="captionText">Texto da descrição:</label>
+                    <textarea id="captionText" value={captionText} onChange={(e) => setCaptionText(e.target.value)} />
+                </div>
+                <button className="form-group" type="submit">Adicionar slide</button>
+            </form>
+        </>
+    );
 }
